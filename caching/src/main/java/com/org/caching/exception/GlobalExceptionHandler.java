de40a49e07b2e2512c1692f7ae4ac5acccb06822
Hemanth328 <hemanth.blueburn11@gmail.com>
2025-06-12 07:47:02 +0530

    Implemented method to carry exceptions for Validations

--- caching/src/main/java/com/org/caching/exception/GlobalExceptionHandler.java
+++ caching/src/main/java/com/org/caching/exception/GlobalExceptionHandler.java
@@ -1,14 +1,42 @@
 package com.org.caching.exception;
 
 import com.org.caching.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> validationErrors = new HashMap<>();
+
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach( error -> {
+
+            String fieldName = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            validationErrors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.badRequest().body(validationErrors);
+    }
 
     @ExceptionHandler(NoBookFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleNoBookFoundException(NoBookFoundException exception, WebRequest request) {
